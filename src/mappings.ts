bdfba42d57107b2fd6649af10c585ca88a5daa5e
benber86 <benoitb@tutanota.de>
2022-11-27 11:03:53 +1100

    First commit

---
+++ src/mappings.ts
@@ -0,0 +1,13 @@
+import { BribeV3, NewTokenReward, PeriodUpdated, RewardAdded } from '../generated/BribeV3/BribeV3'
+
+export function handleNewTokenReward(event: NewTokenReward): void {
+  return
+}
+
+export function handlePeriodUpdated(event: PeriodUpdated): void {
+  return
+}
+
+export function handleRewardAdded(event: RewardAdded): void {
+  return
+}
