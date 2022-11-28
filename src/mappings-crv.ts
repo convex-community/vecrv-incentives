79092819b902c6090f47cc3b4043639da2c30370
benber86 <benoitb@tutanota.de>
2022-11-28 23:04:46 +1100

    Handle bribe.crv.finance

--- src/mappings-crv.ts
+++ src/mappings-crv.ts
@@ -1,5 +1,22 @@
 import { BribeV3, RewardAdded } from '../generated/BribeV3/BribeV3'
+import { getGauge, getPlatform } from './services'
+import { getIntervalFromTimestamp, WEEK } from './utils'
+import { addBribe } from './ybribe'
+
+const PLATFORM_NAME = 'bribe.crv.finance'
 
 export function handleRewardAdded(event: RewardAdded): void {
-  return
+  const platform = getPlatform(PLATFORM_NAME)
+  const gauge = getGauge(event.params.gauge.toHexString())
+  const week = getIntervalFromTimestamp(event.block.timestamp, WEEK)
+  addBribe(
+    platform,
+    event.address,
+    gauge,
+    week,
+    event.params.amount,
+    event.params.reward_token,
+    event.transaction.hash,
+    event.transaction.from
+  )
 }
